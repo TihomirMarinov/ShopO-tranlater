a4f8f4696d4dea77f893c36836afb6b51304d90e
Tihomir Marinov <tihomir.marinov96@gmail.com>
2019-07-10 17:23:08 +0300

    Init project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TopNav extends React.Component{
+    render(){
+        return(
+            <div id="test">
+                <h1>ShopO Translator</h1>
+                <InputForm />
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class InputForm extends React.Component{
+    render(){
+        return(
+            <div className="form">
+                <form>
+                    <input type="file" id="input" multiple="multiple"></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+class SiteStyle extends React.Component{
+    render(){
+        return(
+            <TopNav />
+        );
+    }
+}
+
+ReactDOM.render(<SiteStyle />, document.getElementById("root"));
