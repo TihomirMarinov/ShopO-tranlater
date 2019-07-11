e5f06cf819ead91d69070e843572d615c7692925
Tihomir Marinov <tihomir.marinov96@gmail.com>
2019-07-11 18:08:06 +0300

    Start add js on react

--- var/app.js
+++ var/app.js
@@ -1,13 +1,26 @@
 var input = document.querySelector("input");
-  input.addEventListener("change", function() {
-    Array.prototype.forEach.call(input.files, function(file) {
-      var reader = new FileReader();
-      reader.addEventListener("load", function() {
-        var doc = new DOMParser().parseFromString(reader.result, "application/xml");
-        console.log(doc);
-      });
-      reader.readAsText(file);
-    });
-  });
+var CFG = {
+  item: 'trans-unit',
+  key: 'id',
+  source: 'source',
+  target: 'target',
+  note: 'note',
+};
+input.addEventListener("change", function() {
 
-  
+  Array.prototype.forEach.call(input.files, function(file) {
+    reader.addEventListener("load", function() {
+      var doc = new DOMParser().parseFromString(reader.result, "application/xml");
+
+      console.log(doc);
+
+      xliff2js(doc, (err, res) => {
+        console.log("Result: " + res);
+        console.log("Error: " + err);
+      });
+
+    });
+    console.log(result);
+    reader.readAsText(file);
+  });
+});
